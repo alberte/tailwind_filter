c365076ff123b5b6ee5ff221ff13a0c07b27b624
alberte <niklas.renken@freun.de>
2022-04-14 00:17:55 +0200

    added overrides

--- lib/tailwind_filter.ex
+++ lib/tailwind_filter.ex
@@ -1,54 +1,78 @@
 defmodule TailwindFilter do
   use Memoize
+  alias TailwindFilter.ClassGroups
+
   @moduledoc """
   Documentation for `TailwindFilter`.
+  """
+  @doc """
+  takes a class string (space seperated) and outputs it filtered for tailwind property duplications
+
+  ## Example
+  iex> TailwindFilter.filter("bg-red-200 p-2 hover:bg-blue-100 hover:p-3 hover:bg-yellow-800")
+  "bg-red-200 p-2 hover:p-3 hover:bg-yellow-800"
+  iex> TailwindFilter.filter("mx-2 m-3")
+  "m-3"
+  iex> TailwindFilter.filter("m-3 mx-2")
+  "m-3 mx-2"
   """
   defmemo filter(class_string) when is_binary(class_string) do
     class_string
     |> String.split()
     |> Enum.reverse()
-    |> filter_classes("")
-    |> String.trim()
+    |> filter_classes([])
+    |> Enum.join(" ")
 
+    # |> String.trim()
   end
 
-  defp filter_classes([], resulting_classes) do
+  @doc """
+  ## Examples
+  iex> TailwindFilter.filter_classes([], [])
+  []
+  iex> TailwindFilter.filter_classes(["bg-red-200", "hover:m-2", "hover:m-3", "m-4", "bg-blue-200"], [])
+  [ "m-4", "hover:m-2", "bg-red-200" ]
+  """
+
+  def filter_classes([], resulting_classes) do
     resulting_classes
   end
 
-  defp filter_classes([head | []], resulting_classes) do
-    "#{head} #{resulting_classes}"
+  def filter_classes([head | tail], resulting_classes) do
+    {prefix, property} = ClassGroups.parse(head)
+    group = ClassGroups.find_group(property)
+    tail = Enum.filter(tail, &is_different_group(&1, {prefix, group}))
+    filter_classes(tail, [head | resulting_classes])
   end
 
-  defp filter_classes([head | tail], resulting_classes) do
-    group = find_group(head)
-    tail = Enum.filter(tail, &is_different_group(&1, group))
-    filter_classes(tail, "#{head} #{resulting_classes}")
-  end
+  @doc """
+  ## Examples
+  iex> TailwindFilter.is_different_group("m-2", {"hover:", nil})
+  true
+  iex> TailwindFilter.is_different_group("m-2", {"hover:", {~r"m-[[:digit:]]+", []}})
+  true
+  iex> TailwindFilter.is_different_group("m-2", {"", {~r"m-[[:digit:]]+", []}})
+  false
+  iex> TailwindFilter.is_different_group("m-2", {"hover:", {~r"p-[[:digit:]]+", []}})
+  true
+  iex> TailwindFilter.is_different_group("m-2", {"", {~r"p-[[:digit:]]+", []}})
+  true
+  """
 
-  defp find_group(class) do
-    group =
-      Enum.find(TailwindFilter.ClassGroups.class_groups(), nil, fn class_group ->
-        String.match?(class, class_group)
-      end)
-
-    if group do
-      %{"pre" => pre} = Regex.named_captures(group, class)
-
-      group
-      |> Regex.source()
-      |> String.replace(TailwindFilter.ClassGroups.pre_pattern(), pre)
-      |> Regex.compile!()
-    else
-      nil
-    end
-  end
-
-  defp is_different_group(_class, nil) do
+  def is_different_group(_class, {_, nil}) do
     true
   end
 
-  defp is_different_group(class, group) do
-    !String.match?(class, group)
+  require Logger
+
+  def is_different_group(class, {original_prefix, {original_regex, overrides}}) do
+    {prefix, prop} = ClassGroups.parse(class)
+
+    !(prefix == original_prefix &&
+        (String.match?(prop, original_regex) ||
+           Enum.any?(overrides, fn override ->
+             {override_regex, _} = ClassGroups.class_groups()[override]
+             String.match?(prop, override_regex)
+           end)))
   end
 end
--- lib/tailwind_filter/class_groups.ex
+++ lib/tailwind_filter/class_groups.ex
@@ -1,18 +1,35 @@
 defmodule TailwindFilter.ClassGroups do
+  @tw_class_pattern ~r"(?<pre>[!]?([a-z:]+:)?)(?<prop>.+)"
 
-
+  @doc """
+  A map of tailwind class groups.
+  Each group is a tuple consisting of two elements:
+  1. the regex for matching the group itself
+  2. a list of keys of other groups which are overwritten by the group as well.
+  """
   def class_groups do
-    [
-      ~r"#{beginning_pattern()}#{pre_pattern()}bg-[a-z]+-\d+",
-      ~r"#{beginning_pattern()}#{pre_pattern()}m-\d+"
-    ]
+    %{
+      margin: {~r"m-[[:digit:]]+", [:margin_x, :margin_y]},
+      margin_x: {~r"mx-[[:digit:]]+", []},
+      margin_y: {~r"my-[[:digit:]]+", []},
+      bg_color: {~r"bg-[a-z]+-[[:digit:]]+", []}
+    }
   end
 
-  def pre_pattern do
-    "(?<pre>([a-z:]+:)?)"
+  def find_group(class) do
+    {_key, group} =
+      Enum.find(class_groups(), {nil, nil}, fn {_key, {regex, _}} ->
+        Regex.match?(regex, class)
+      end)
+
+    group
   end
 
-  def beginning_pattern do
-    "(^|\s+)"
+  @doc """
+    returns a tuple containing the prefix (before the ":") and the tailwind property itself (e.g. "bg-red-200")
+  """
+  def parse(class) do
+    %{"pre" => pre, "prop" => prop} = Regex.named_captures(@tw_class_pattern, class)
+    {pre, prop}
   end
 end
